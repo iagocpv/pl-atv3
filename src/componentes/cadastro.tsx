20c9711890e06baec1d2d9cee81f6e4f5e81468a
IagoSouza <iago.csouza15@gmail.com>
2023-12-03 18:19:34 -0300

    alterando componente de cadastros

--- src/componentes/cadastro.tsx
+++ src/componentes/cadastro.tsx
@@ -1,24 +1,21 @@
-import { Component } from "react";
 import "./style.css"
 
 type props = {
     seletorView: Function
 }
 
-export default class Cadastro extends Component<props>{
-
-    render() {
-        return(
-            <>
-                <h3 className="cadastro-title">O que deseja cadastrar?</h3>
-                <div className="opcoes-cadastro">
-                    <button onClick={(e) => this.props.seletorView('CadastroCliente', e)}>Clientes</button>
-                    <button onClick={(e) => this.props.seletorView('CadastroPet', e)}>Pets</button>
-                    <button onClick={(e) => this.props.seletorView('CadastroProduto', e)}>Produtos</button>
-                    <button onClick={(e) => this.props.seletorView('CadastroServiço', e)}>Serviços</button>
-                </div>
-            </>
-        )
-    }
-
+function Cadastro(props: props) {
+    return(
+        <>
+            <h3 className="cadastro-title">O que deseja cadastrar?</h3>
+            <div className="opcoes-cadastro">
+                <button onClick={(e) => props.seletorView('CadastroCliente', e)}>Clientes</button>
+                <button onClick={(e) => props.seletorView('CadastroPet', e)}>Pets</button>
+                <button onClick={(e) => props.seletorView('CadastroProduto', e)}>Produtos</button>
+                <button onClick={(e) => props.seletorView('CadastroServiço', e)}>Serviços</button>
+            </div>
+        </>
+    )
 }
+
+export default Cadastro
